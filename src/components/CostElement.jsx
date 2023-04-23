66e0f5057b507b346529f26ef244506b551b3d2d
BaldezPatrick <patrickbaldex@gmail.com>
2023-04-23 20:57:46 -0300

    Improves card reusability.

--- src/components/CostElement.jsx
+++ src/components/CostElement.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import Card from "./Card";
 import CostDate from "./CostDate";
 import "./CostElement.css";
 
 const CostElement = ({ date }) => {
   return (
     <>
-      <div className="cost__item">
+      <Card className="cost__item">
         <CostDate date={date} />
         <div className="cost__desc__item">
           <h2>Car</h2>
           <div className="cost__price__item">$100.000</div>
         </div>
-      </div>
+      </Card>
     </>
   );
 };

d6ada72fc5079b422d54061d22118f3e47704433
BaldezPatrick <patrickbaldex@gmail.com>
2023-04-23 20:26:00 -0300

    Add new style.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CostElement from "./components/CostElement";
 function App() {
   const date = new Date(2019, 2, 3);
   return (
-    <div>
+    <div className="costs">
       <CostElement date={date} />
     </div>
   );

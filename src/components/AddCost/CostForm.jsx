fbdc85d2c6e813ce30335692cfbf26d0af8a4949
BaldezPatrick <patrickbaldex@gmail.com>
2023-04-25 14:52:52 -0300

    States and functions to get the input user.

--- src/components/AddCost/CostForm.jsx
+++ src/components/AddCost/CostForm.jsx
@@ -1,22 +1,49 @@
+import { useState } from "react";
 import "./CostForm.css";
 
 const CostForm = () => {
+  const [inputTitle, setInputTitle] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [inputDate, setInputDate] = useState("");
+
+  const titleChangeHandler = (e) => {
+    setInputTitle(e.target.value);
+  };
+
+  const valueChangeHandler = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const dateCHangeHandler = (e) => {
+    setInputDate(e.target.value);
+  };
+
   return (
     <form>
       <div className="cost__add__controls">
         <div className="cost__add__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
 
         <div className="cost__add__control">
           <label>Value</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={valueChangeHandler}
+          />
         </div>
 
         <div className="cost__add__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
+            onChange={dateCHangeHandler}
+          />
         </div>
       </div>
 

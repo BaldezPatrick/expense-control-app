a08365735abb729947c4ece86c98041306a8d960
BaldezPatrick <patrickbaldex@gmail.com>
2023-04-25 14:11:03 -0300

    Add costs component.

---
+++ src/components/AddCost/AddCost.jsx
@@ -0,0 +1,12 @@
+import "./AddCost.css";
+import CostForm from "./CostForm";
+
+const AddCost = () => {
+  return (
+    <div className="add__cost">
+      <CostForm />
+    </div>
+  );
+};
+
+export default AddCost;

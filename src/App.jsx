f54605aa411014cd9e372215ca47417ce28d2407
BaldezPatrick <patrickbaldex@gmail.com>
2023-04-25 14:10:05 -0300

    Change style card.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import AddCost from "./components/AddCost/AddCost";
 import CostElement from "./components/Costs/CostElement";
 import Card from "./components/Layout/Card";
 
 function App() {
   const date = new Date(2019, 2, 3);
   return (
-    <Card>
-      <CostElement date={date} name={"Bike"} price={2000} />
-    </Card>
+    <>
+      <AddCost />
+      <Card className="cost">
+        <CostElement date={date} name={"Bike"} price={2000} />
+      </Card>
+    </>
   );
 }
 

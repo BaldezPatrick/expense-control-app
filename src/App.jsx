fcd945827346f4704013038f6f01acbeb8c7825f
BaldezPatrick <patrickbaldex@gmail.com>
2023-04-26 23:07:35 -0300

    Add an initial state to use on CostElement.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
+import { useState } from "react";
 import AddCost from "./components/AddCost/AddCost";
 import CostElement from "./components/Costs/CostElement";
-import Card from "./components/Layout/Card";
+
+const initialCosts = [
+  {
+    id: 1,
+    title: "Carro",
+    price: "100.000",
+    date: new Date(),
+  },
+  {
+    id: 2,
+    title: "Casa",
+    price: "250.000",
+    date: new Date("2020-02-21"),
+  },
+];
 
 function App() {
-  const costs = [
-    {
-      title: "Carro",
-      price: "100.000",
-      date: new Date(),
-    },
-  ];
+  const [cost, setCost] = useState(initialCosts);
+
   const addCostHandler = (costs) => {
+    setCost((prevCostsState) => {
+      return [costs, ...prevCostsState];
+    });
     console.log(costs);
   };
 
   return (
     <>
       <AddCost onAddCosts={addCostHandler} />
-      <CostElement costs={costs} />
+      <CostElement costs={cost} />
     </>
   );
 }

2d9eedc612c3adb162e59a94b1a9e5c401fd8703
BaldezPatrick <patrickbaldex@gmail.com>
2023-04-27 17:11:59 -0300

    New responsiveness to mobile.

--- src/components/Diagram/Diagram.jsx
+++ src/components/Diagram/Diagram.jsx
@@ -1,19 +1,56 @@
 import DiagramBar from "./DiagramBar";
 import "./Diagram.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Diagram = ({ dataCosts }) => {
+  const [isShow, setIsShow] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const screenWidthUpdate = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", screenWidthUpdate);
+    return () => {
+      window.removeEventListener("resize", screenWidthUpdate);
+    };
+  }, []);
+
+  const showDiagram = () => {
+    setIsShow(!isShow);
+  };
+  const buttonText = isShow ? "Hide Months" : "Show Months";
+
   const dataCostValues = dataCosts.map((dataCost) => dataCost.value);
   const max = Math.max(...dataCostValues);
+
   return (
     <div className="diagram">
-      {dataCosts.map((dataCost) => (
-        <DiagramBar
-          key={dataCost.label}
-          value={dataCost.value}
-          maxValue={max}
-          label={dataCost.label}
-        />
-      ))}
+      {screenWidth <= 500 ? (
+        <>
+          <button onClick={showDiagram}>{buttonText}</button>
+          {isShow &&
+            dataCosts.map((dataCost) => (
+              <DiagramBar
+                key={dataCost.label}
+                value={dataCost.value}
+                maxValue={max}
+                label={dataCost.label}
+              />
+            ))}
+        </>
+      ) : (
+        dataCosts.map((dataCost) => (
+          <DiagramBar
+            key={dataCost.label}
+            value={dataCost.value}
+            maxValue={max}
+            label={dataCost.label}
+          />
+        ))
+      )}
     </div>
   );
 };
